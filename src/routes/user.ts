8347a6d5f8af989cd5a68d0a48e1bcceff253f39
Alexandre <alexandre.monin03@gmail.com>
2023-12-21 11:21:54 +0100

    Define User routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,30 @@
-import express, { Router } from 'express';
+import express, { Router, Request } from 'express';
 
 //Définir le router
 const router: Router = express.Router();
 
-//Définir les routes
+//Récupérer un utilisateur grâce à son mail
+router.get("/:email", (req: Request<{email: String}>, res) => {
+    const { email }  = req.params;
+})
+
+//Créer un utilisateur
+router.post("/add", (req, res) => {
+
+});
+
+//Modifier un utilisateur
+router.put("/:email", (req: Request<{email: String}>, res) => {
+    const { email }  = req.params;
+
+});
+
+//Supprimer un utilisateur
+router.delete("/:email", (req: Request<{email: String}>, res) => {
+    const { email }  = req.params;
+
+});
+
 
 //Exporter le router
 export default router;
