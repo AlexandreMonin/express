f80e0129002640a94ed46cddded52afeb2a4a619
Alexandre <alexandre.monin03@gmail.com>
2023-12-21 11:33:13 +0100

    Define Command routes

--- src/routes/command.ts
+++ src/routes/command.ts
@@ -1,9 +1,30 @@
-import express, { Router } from 'express';
+import express, { Router, Request } from 'express';
 
 //Définir le router
 const router: Router = express.Router();
 
-//Définir les routes
+//Récupérer une commande grâce à son id
+router.get("/:id", (req: Request<{id: Number}>, res) => {
+    const { id }  = req.params;
+
+});
+
+//Créer une commande
+router.post("/add", (req, res) => {
+
+});
+
+//Modifier une commande
+router.put("/:id", (req: Request<{id: Number}>, res) => {
+    const { id }  = req.params;
+    
+});
+
+//Supprimer une commande
+router.delete("/:id", (req: Request<{id: Number}>, res) => {
+    const { id }  = req.params;
+    
+});
 
 //Exporter le router
 export default router;
